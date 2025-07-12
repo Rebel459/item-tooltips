d98f39138aa1d39f0b8999febd6683b6eafc4fd0
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-07-12 12:32:31 +1000

    Revert "client"
    This reverts commit 2d8d5b4b826b303eefdfb3e5c91c591cfb4cf857.

---
+++ src/main/java/net/legacy/item_tooltips/mixin/item/ItemMixin.java
@@ -0,0 +1,37 @@
+package net.legacy.item_tooltips.mixin.item;
+
+import net.legacy.item_tooltips.ItemTooltips;
+import net.legacy.item_tooltips.config.ITConfig;
+import net.legacy.item_tooltips.registry.ITItemTags;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(Item.class)
+public abstract class ItemMixin {
+
+    @Inject(method = "appendHoverText", at = @At(value = "HEAD"))
+    private void addShiftDescription(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag, CallbackInfo ci) {
+        if (itemStack.is(ITItemTags.HAS_DESCRIPTION)) {
+            MutableComponent prefixText = Component.translatable(ITConfig.get.tooltip_prefix).withColor(ITConfig.get.tooltip_prefix_color);
+            MutableComponent descriptionText = Component.translatable(itemStack.getItem().getDescriptionId() + ".desc").withColor(ITConfig.get.tooltip_color);
+            if (ITConfig.get.require_shift) {
+                if (Screen.hasShiftDown()) {
+                    list.add(Component.translatable("").append(prefixText).append(descriptionText));
+                }
+                else if (ITConfig.get.tooltip_notice)
+                    list.add(Component.translatable("tooltip." + ItemTooltips.MOD_ID + ".hold_shift").withColor(ITConfig.get.tooltip_color));
+            }
+            else list.add(Component.translatable("").append(prefixText).append(descriptionText));
+        }
+    }
+}
