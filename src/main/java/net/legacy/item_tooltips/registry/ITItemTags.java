3c51b43e4539e35f930a1648b6f356ad832538df
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-07-13 02:11:09 +1000

    extra description lines

--- src/main/java/net/legacy/item_tooltips/registry/ITItemTags.java
+++ src/main/java/net/legacy/item_tooltips/registry/ITItemTags.java
@@ -9,6 +9,10 @@ import org.jetbrains.annotations.NotNull;
 public class ITItemTags {
 
     public static final TagKey<Item> HAS_DESCRIPTION = bind("has_description");
+    public static final TagKey<Item> HAS_DESCRIPTION_SECOND = bind("has_description_second_line");
+    public static final TagKey<Item> HAS_DESCRIPTION_THIRD = bind("has_description_third_line");
+    public static final TagKey<Item> HAS_DESCRIPTION_FOURTH = bind("has_description_fourth_line");
+    public static final TagKey<Item> HAS_DESCRIPTION_FIFTH = bind("has_description_fifth_line");
 
     @NotNull
     private static TagKey<Item> bind(@NotNull String path) {
