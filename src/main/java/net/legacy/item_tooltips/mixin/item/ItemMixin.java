3a8b2ae96bdbbd4641b8228ae0a9c57b2ba3c4cb
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-09-06 23:23:31 +1000

    backport v1.3

--- src/main/java/net/legacy/item_tooltips/mixin/item/ItemMixin.java
+++ src/main/java/net/legacy/item_tooltips/mixin/item/ItemMixin.java
@@ -1,28 +1,33 @@
 package net.legacy.item_tooltips.mixin.item;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.legacy.item_tooltips.ItemTooltips;
 import net.legacy.item_tooltips.config.ITConfig;
 import net.legacy.item_tooltips.registry.ITItemTags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
-import java.util.function.Consumer;
 
+@Environment(EnvType.CLIENT)
 @Mixin(Item.class)
 public abstract class ItemMixin {
 
     @Shadow public abstract ItemStack getDefaultInstance();
+
+    @Unique
+    public boolean hasShiftNotice = false;
 
     @Inject(method = "appendHoverText", at = @At(value = "HEAD"))
     private void addDescription(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag, CallbackInfo ci) {
@@ -31,12 +36,26 @@ public abstract class ItemMixin {
             MutableComponent prefixText = Component.translatable(ITConfig.get.descriptions.prefix).withColor(ITConfig.get.descriptions.prefix_color);
             MutableComponent descriptionText = Component.translatable(this.getDefaultInstance().getDescriptionId() + ".desc").withColor(ITConfig.get.descriptions.color);
             if (ITConfig.get.descriptions.require_shift) {
-                if (Screen.hasShiftDown())
-                    list.add(Component.translatable("").append(prefixText).append(descriptionText));
-                else if (ITConfig.get.descriptions.shift_notice && !this.getDefaultInstance().is(ITItemTags.NO_SHIFT_NOTICE))
+                if (Screen.hasShiftDown()) {
+                    list.add(Component.literal("").append(prefixText).append(descriptionText));
+                    this.hasShiftNotice = false;
+                }
+                else if (ITConfig.get.descriptions.shift_notice && !this.getDefaultInstance().is(ITItemTags.NO_SHIFT_NOTICE)) {
                     list.add(Component.translatable("tooltip." + ItemTooltips.MOD_ID + ".hold_shift").withColor(ITConfig.get.descriptions.color));
-                else list.add(Component.translatable("").append(prefixText).append(descriptionText));
+                    this.hasShiftNotice = true;
+                }
+            }
+            else {
+                list.add(Component.literal("").append(prefixText).append(descriptionText));
+                this.hasShiftNotice = false;
             }
         }
+    }
+
+    @Inject(method = "appendHoverText", at = @At(value = "HEAD"))
+    private void addEnchantmentShiftNotice(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag, CallbackInfo ci) {
+        if (!ItemTooltips.enchantmentTooltips || !ITConfig.get.enchantments.require_shift || !ITConfig.get.enchantments.shift_notice) return;
+        if (Screen.hasShiftDown()) list.add(Component.literal(""));
+        else if (!this.hasShiftNotice) list.add(Component.translatable("tooltip." + ItemTooltips.MOD_ID + ".hold_shift").withColor(ITConfig.get.descriptions.color));
     }
 }
