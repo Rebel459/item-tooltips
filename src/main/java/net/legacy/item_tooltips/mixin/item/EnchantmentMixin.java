3aa4e664932869344e40db7e4fd9376de6621cc6
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-01-24 11:03:02 +1100

    v1.5.2

--- src/main/java/net/legacy/item_tooltips/mixin/item/EnchantmentMixin.java
+++ src/main/java/net/legacy/item_tooltips/mixin/item/EnchantmentMixin.java
@@ -7,10 +7,7 @@ import net.legacy.item_tooltips.config.ITConfig;
 import net.legacy.item_tooltips.tag.ITEnchantmentTags;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.Holder;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.ComponentUtils;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.*;
 import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.world.item.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,22 +16,24 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
-@Mixin(value = Enchantment.class, priority = 1)
+@Mixin(value = Enchantment.class)
 public abstract class EnchantmentMixin {
 
     @Inject(method = "getFullname", at = @At("TAIL"), cancellable = true)
-    private static void recolourEnchantments(Holder<Enchantment> enchantment, int i, CallbackInfoReturnable<Component> cir) {
+    private static void recolourEnchantments(Holder<Enchantment> enchantment, int level, CallbackInfoReturnable<Component> cir) {
         MutableComponent mutableComponent = enchantment.value().description().copy();
-        if (enchantment.is(ITEnchantmentTags.BLESSING) && (ITConfig.get.enchantments.names.blessing_color != ITConfig.Defaults.BLESSING_COLOR) || !FabricLoader.getInstance().isModLoaded("enchants_and_expeditions")) {
-            mutableComponent = ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.blessing_color));
+        if (level > 1) mutableComponent.append(CommonComponents.SPACE).append(Component.translatable("enchantment.level." + level));
+
+        if (enchantment.is(ITEnchantmentTags.BLESSING)) {
+            ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.blessing_color));
             cir.setReturnValue(mutableComponent);
         }
-        else if (enchantment.is(EnchantmentTags.CURSE) && ITConfig.get.enchantments.names.curse_color != ITConfig.Defaults.CURSE_COLOR) {
-            mutableComponent = ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.curse_color));
+        else if (enchantment.is(EnchantmentTags.CURSE)) {
+            ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.curse_color));
             cir.setReturnValue(mutableComponent);
         }
-        else if (ITConfig.get.enchantments.names.enchantment_color != ITConfig.Defaults.ENCHANTMENT_COLOR) {
-            mutableComponent = ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.enchantment_color));
+        else if (!enchantment.is(ITEnchantmentTags.BLESSING) && !enchantment.is(EnchantmentTags.CURSE)) {
+            ComponentUtils.mergeStyles(mutableComponent, Style.EMPTY.withColor(ITConfig.get.enchantments.names.enchantment_color));
             cir.setReturnValue(mutableComponent);
         }
     }
